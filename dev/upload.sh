a1edc4dafc1b069febcefa7cea9408b93fdb0b5b
Katsutoshi Seki <sekika@users.noreply.github.com>
2025-02-06 20:47:30 +0900

    Release 1.1.0

--- dev/upload.sh
+++ dev/upload.sh
@@ -6,6 +6,7 @@ cd `echo $0 | sed -e 's/[^/]*$//'`
 ./test.sh
 
 # Make package
+python3 -m pip install build
 echo "Making packages."
 cd ..
 python3 -m build
@@ -13,7 +14,7 @@ python3 -m build
 # Token required. Check ~/.pypirc
 twine upload --skip-existing dist/*
 
-# Uninstall unsatfit
+# Uninstall multiai
 python3 -m pip uninstall multiai
 echo "Upload completed. Installed version uninstalled. Wait for a while and run"
 echo "python3 -m pip install multiai"
